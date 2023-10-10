fe6868cc5227b281a391ad4538a9902d47e2f5cd
claud <mrclaudlive@gmail.com>
2023-10-10 15:46:16 +0300

    remove old murkups

--- markups.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
-
-
-
-
-
-# main_mk = ReplyKeyboardBuilder()
-# main_mk.button(text="test")
